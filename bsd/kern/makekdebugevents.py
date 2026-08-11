2d943bcbaefc63dbd8228a9726196dec34186148
Gareth Davidson <gaz@bitplane.net>
2026-08-11 14:16:12 +0100

    Run makekdebugevents with Python 3

--- bsd/kern/makekdebugevents.py
+++ bsd/kern/makekdebugevents.py
@@ -15,23 +15,23 @@ if (len(sys.argv) < 2):
 trace_code_file = sys.argv[1]
 
 # regular expression pattern to match <hex_id> <string>
-id_name_pattern = re.compile('0x([0-9a-fA-F]+)\s+([^\s]*)')
+id_name_pattern = re.compile(r'0x([0-9a-fA-F]+)\s+([^\s]*)')
 code_table = []
 
 # scan file to generate internal table
 with open(trace_code_file, 'rt') as codes:
     for line in codes:
-	m = id_name_pattern.match(line)
-	if m:
+        m = id_name_pattern.match(line)
+        if m:
             code_table += [(int(m.group(1),base=16), m.group(2))]
 
 # emit typedef:
-print "typedef struct {"
-print "        uint32_t   id;"
-print "        const char *name;"
-print "} kd_event_t;"
+print("typedef struct {")
+print("        uint32_t   id;")
+print("        const char *name;")
+print("} kd_event_t;")
 # emit structure declaration and sorted initialization:
-print "kd_event_t kd_events[] = {"
+print("kd_event_t kd_events[] = {")
 for mapping in sorted(code_table, key=lambda x: x[0]):
-        print "        {0x%x, \"%s\"}," % mapping
-print "};"
+        print("        {0x%x, \"%s\"}," % mapping)
+print("};")
