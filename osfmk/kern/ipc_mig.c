3365c5b53c1504f0477d14d5d7ce1d48ca423d5a
Gareth Davidson <gaz@bitplane.net>
2026-08-11 16:06:38 +0100

    Provide kernel MIG message destruction

--- osfmk/kern/ipc_mig.c
+++ osfmk/kern/ipc_mig.c
@@ -493,6 +493,69 @@ mach_msg_rpc_from_kernel_body(
 	return mr;
 }
 
+void
+mach_msg_destroy_from_kernel_proper(mach_msg_header_t *msg)
+{
+	mach_msg_bits_t mbits = msg->msgh_bits;
+	ipc_object_t object;
+
+	object = (ipc_object_t)msg->msgh_remote_port;
+	if (IO_VALID(object)) {
+		ipc_object_destroy(object, MACH_MSGH_BITS_REMOTE(mbits));
+	}
+
+	assert(!MACH_MSGH_BITS_VOUCHER(mbits));
+	if ((mbits & MACH_MSGH_BITS_COMPLEX) == 0) {
+		return;
+	}
+
+	mach_msg_body_t *body = (mach_msg_body_t *)(msg + 1);
+	mach_msg_descriptor_t *daddr = (mach_msg_descriptor_t *)(body + 1);
+	mach_msg_size_t i;
+
+	for (i = 0; i < body->msgh_descriptor_count; i++, daddr++) {
+		switch (daddr->type.type) {
+		case MACH_MSG_PORT_DESCRIPTOR: {
+			mach_msg_port_descriptor_t *dsc = &daddr->port;
+			if (IO_VALID((ipc_object_t)dsc->name)) {
+				ipc_object_destroy((ipc_object_t)dsc->name, dsc->disposition);
+			}
+			break;
+		}
+		case MACH_MSG_OOL_VOLATILE_DESCRIPTOR:
+		case MACH_MSG_OOL_DESCRIPTOR: {
+			mach_msg_ool_descriptor_t *dsc = &daddr->out_of_line;
+			if (dsc->size > 0) {
+				vm_map_copy_discard((vm_map_copy_t)dsc->address);
+			} else {
+				assert(dsc->address == (void *)0);
+			}
+			break;
+		}
+		case MACH_MSG_OOL_PORTS_DESCRIPTOR: {
+			mach_msg_ool_ports_descriptor_t *dsc = &daddr->ool_ports;
+			ipc_object_t *objects = (ipc_object_t *)dsc->address;
+			mach_msg_type_number_t j;
+
+			if (dsc->count == 0) {
+				break;
+			}
+			assert(objects != 0);
+			for (j = 0; j < dsc->count; j++) {
+				object = objects[j];
+				if (IO_VALID(object)) {
+					ipc_object_destroy(object, dsc->disposition);
+				}
+			}
+			kfree(dsc->address,
+			    (vm_size_t)dsc->count * sizeof(mach_port_t));
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
 
 /************** These Calls are set up for kernel-loaded tasks/threads **************/
 
